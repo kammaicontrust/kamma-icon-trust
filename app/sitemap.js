72a1af940759e26d550c3a9fded0ae61a9691a86
kamma-icon-trust <kammaicontrust@gmail.com>
2026-02-20 23:03:39 +0530

    added sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,23 @@
 export default function sitemap() {
   return [
     {
-      url: "https://kammaicontrust.org",
+      url: "https://kammaicontrust.org/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kammaicontrust.org/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kammaicontrust.org/initiatives",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kammaicontrust.org/gallery",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kammaicontrust.org/support",
       lastModified: new Date(),
     },
   ];
