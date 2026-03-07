6c8f579e40185a06703d03553657c9ab2fe5b1c7
kamma-icon-trust <kammaicontrust@gmail.com>
2026-03-07 15:10:32 +0530

    pic updated

--- app/components/LaunchBanner.jsx
+++ app/components/LaunchBanner.jsx
@@ -27,7 +27,7 @@ export default function LaunchBanner() {
         </h1>
 
         <img
-          src="public\sridhar.png"
+          src="public/sridhar.png"
           className="launch-photo"
           alt="Chief Guest"
         />
