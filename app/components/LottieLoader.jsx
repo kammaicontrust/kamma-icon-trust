e30f210eab58eb698820127fb1b2ec2c9e409292
kamma-icon-trust <kammaicontrust@gmail.com>
2026-03-10 02:14:27 +0530

    loader updated

--- app/components/LottieLoader.jsx
+++ app/components/LottieLoader.jsx
@@ -1,30 +1,35 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DotLottie } from "@lottiefiles/dotlottie-web";
 
-export default function LottieLoader() {
+export default function LottieLoader({ children }) {
 
   const canvasRef = useRef(null);
+  const [loading,setLoading] = useState(true);
 
-  useEffect(() => {
+  useEffect(()=>{
 
-    const dotLottie = new DotLottie({
-      autoplay: true,
-      loop: true,
-      canvas: canvasRef.current,
-      src: "https://lottie.host/ec31ea4d-78c9-4650-8274-7aa9cfe05cfb/vkRocbFKRe.lottie"
+    new DotLottie({
+      autoplay:true,
+      loop:true,
+      canvas:canvasRef.current,
+      src:"https://lottie.host/ec31ea4d-78c9-4650-8274-7aa9cfe05cfb/vkRocbFKRe.lottie"
     });
 
-  }, []);
+    setTimeout(()=>{
+      setLoading(false);
+    },3000);
 
-  return (
-    <div className="loader-screen">
-      <canvas
-        ref={canvasRef}
-        style={{ width: "300px", height: "300px" }}
-      ></canvas>
-    </div>
-  );
+  },[]);
 
+  if(loading){
+    return(
+      <div className="loader-screen">
+        <canvas ref={canvasRef} style={{width:"320px",height:"320px"}}/>
+      </div>
+    )
+  }
+
+  return children;
 }
