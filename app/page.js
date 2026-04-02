df6a68f490c076c8d0e7cda188fc3d09de4819d3
kamma-icon-trust <kammaicontrust@gmail.com>
2026-04-02 11:07:05 +0530

    video

--- app/page.js
+++ app/page.js
@@ -301,12 +301,12 @@ export default function Home() {
 
 
 
-{/* <VideosSection /> */}
+<VideosSection />
 
-{/* SMOOTH DIVIDER (Videos → Gallery)
+{/* SMOOTH DIVIDER (Videos → Gallery) */}
 <div className="w-full h-20 bg-gradient-to-b from-black/0 to-black relative z-20"></div>
 
-<Divider /> */}
+<Divider />
 
 
 
